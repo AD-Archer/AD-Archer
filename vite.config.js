7bb6dc7f2a51e8bcd9c8eaf243394c38e9cc547d
Archer <89430358+AD-Archer@users.noreply.github.com>
2025-02-09 13:26:35 -0500

    Update viteconfig

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 import { viteStaticCopy } from 'vite-plugin-static-copy'
 import path from 'path'
 
+// Use import.meta.url to resolve paths in Vite
+const resolvePath = (...segments) => path.resolve(new URL('.', import.meta.url).pathname, ...segments);
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
@@ -24,7 +27,7 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src')  // Point @ to the src directory this will make it easier for me to find components
+      '@': resolvePath('src')
     }
   },
   build: {
