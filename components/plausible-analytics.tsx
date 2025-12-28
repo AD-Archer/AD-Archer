b4c74ad9698fee4e4d33d10f17d51438b87d6c53
ad-archer <antonioarcher.dev@gmail.com>
2025-12-28 08:16:00 -0500

    minor fixes

--- components/plausible-analytics.tsx
+++ components/plausible-analytics.tsx
@@ -8,10 +8,19 @@ export default function PlausibleAnalytics() {
     if (typeof window !== 'undefined') {
       import('@plausible-analytics/tracker').then(({ init }) => {
         init({
-          domain: 'plausible.adarcher.app',
+          domain: 'antonioarcher.com',
+          endpoint: 'https://plausible.adarcher.app/api/event',
           autoCapturePageviews: true,
+          hashBasedRouting: false,
           outboundLinks: true,
           fileDownloads: true,
+          formSubmissions: true,
+          captureOnLocalhost: false,
+          logging: true,
+          customProperties: {
+            content_category: 'portfolio',
+          },
+          bindToWindow: true,
         });
       });
     }
